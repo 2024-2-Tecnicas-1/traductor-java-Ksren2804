bf70d6e6722baf6130ddd843da0faf23f8a6a8b7
Ksren2804 <karenbayonaromero@gmail.com>
2024-09-02 23:30:09 -0500

    Traductor Javaaa

---
+++ TraductorJavaaa/src/main/java/com/mycompany/traductorjavaaa/TraductorJavaaa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.traductorjavaaa;
+
+/**
+ *
+ * @author User
+ */
+public class TraductorJavaaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
